4bf8188d01fc5a2b9cdc0a653eb4f9832260fbd1
Prabal Manchanda <manchapr@sheridancollege.ca>
2023-02-01 17:13:56 -0500

    parttime student class is added

---
+++ src/javasoftware/PartTimeStudent.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javasoftware;
+
+/**
+ *
+ * @author manch
+ */
+public class PartTimeStudent extends JavaSoftware {
+    private int noofcourse;
+
+    public PartTimeStudent(int noofcourse, String studentname) {
+        this.noofcourse = noofcourse;
+    }
+
+    public int getNoofcourse() {
+        return noofcourse;
+    }
+
+    public void setNoofcourse(int noofcourse) {
+        this.noofcourse = noofcourse;
+    }
+    
+}
